293101d2533e1b221471b299905f24d8d00e7ff7
Jonah Ominyi <jonahjohnzon@gmail.com>
2024-01-29 11:50:09 -0800

    data

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
     ur = "https://jsonplaceholder.typicode.com/"
     user = requests.get(ur + "users/{}".format(sys.argv[1])).json()
     todo = requests.get(ur + "todos", params={"userId": sys.argv[1]}).json()
-    
+
     completed = [t.get("title") for t in todo if t.get("completed") is True]
     print("Employee {} is done with tasks({}/{}):".format(
         user.get("name"), len(completed), len(todo)))
